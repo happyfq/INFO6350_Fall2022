d223985781b88e74cbb3f31cc4530269411d3531
happyfq <651408115@qq.com>
2022-09-18 21:37:40 -0700

    Assignment1

--- Multiply Numbers/Multiply Numbers/ViewController.swift
+++ Multiply Numbers/Multiply Numbers/ViewController.swift
@@ -2,18 +2,23 @@
 //  ViewController.swift
 //  Multiply Numbers
 //
-//  Created by 醒着像睡着 on 9/18/22.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtFirstNumber: UITextField!
+    
+    @IBOutlet weak var txtSecondNumber: UITextField!
+    @IBOutlet weak var IblResult: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func MultiplyFunction(_ sender: Any) {
+        let firstNumber=Int(txtFirstNumber.text ?? "0") ?? 0
+        let secondNumber=Int(txtSecondNumber.text ?? "0") ?? 0
+        let product=firstNumber*secondNumber
+        IblResult.text=String(product)
+    }
 }
-
