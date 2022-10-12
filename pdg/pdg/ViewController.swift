906b149629b770123df071d3fcb2699229803989
happyfq <651408115@qq.com>
2022-10-11 21:49:02 -0700

    Checking in code for using QR code scanner

---
+++ pdg/pdg/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pdg
+//
+//  Created by Fq W on 10/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
