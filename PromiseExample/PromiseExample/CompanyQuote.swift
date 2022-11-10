677627b6a9a538ea1f3893f66e4f7089deebc129
happyfq <651408115@qq.com>
2022-11-09 20:50:29 -0800

    getStockPrice

---
+++ PromiseExample/PromiseExample/CompanyQuote.swift
@@ -0,0 +1,14 @@
+//
+//  CompanyQuote.swift
+//  PromiseExample
+//
+//  Created by Fq W on 11/9/22.
+//
+
+import Foundation
+
+class CompanyQuote{
+    var symbol : String = ""
+    var name : String = ""
+    var price : Float = 0.0
+}
