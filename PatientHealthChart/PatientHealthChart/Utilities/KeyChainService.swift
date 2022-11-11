fe9d726c7351017a9fe939ef346e4fb8c973c124
happyfq <651408115@qq.com>
2022-11-10 22:07:58 -0800

    Patient Health Chart

---
+++ PatientHealthChart/PatientHealthChart/Utilities/KeyChainService.swift
@@ -0,0 +1,25 @@
+//
+//  KeychainService.swift
+//  PatientHealthChart
+//
+//  Created by Fq W on 11/10/22.
+//
+
+import Foundation
+import KeychainSwift
+
+class KeyChainService{
+    var _keyChain = KeychainSwift()
+    
+    var keyChain: KeychainSwift {
+        get {
+            return _keyChain
+        }
+        set {
+            _keyChain = newValue
+        }
+    }
+    
+}
+
+
